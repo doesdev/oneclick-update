d8407c5b290412a33b51fc2cb29863126d500472
Andrew Carpenter <andrew@audioinhd.com>
2019-02-09 22:33:28 -0500

    Go back to one file, just because

--- index.js
+++ index.js
@@ -1,9 +1,303 @@
 'use strict'
 
-const repos = require('jao').repos = {}
-const { getConfig, getServerUrl } = require('./lib/config')
-const { getChannel, latestByChannel } = require('./lib/channels')
-const { getPlatform, getPlatformAsset } = require('./lib/platforms')
+const { get: httpsGet } = require('https')
+const semver = require('semver')
+const repos = {}
+
+const {
+  GITHUB_ACCOUNT,
+  GITHUB_PROJECT,
+  GITHUB_REPO,
+  GITHUB_OAUTH_TOKEN,
+  SERVER_URL
+} = process.env
+
+const hdrUa = { 'User-Agent': `oneclick-update` }
+const hdrGhJson = { Accept: `application/vnd.github.v3+json` }
+const uaJsonHeader = Object.assign({}, hdrUa, hdrGhJson)
+
+const setUaJson = (h) => Object.assign({}, h, hdrUa, hdrGhJson)
+const getAuthHdr = (token) => (token ? { Authorization: `token ${token}` } : {})
+const getAuthHdrJson = (token) => setUaJson(getAuthHdr(token))
+
+const apiBaseUrl = (repo) => `https://api.github.com/repos/${repo}`
+
+class GithubApiError extends Error {
+  constructor (message, res) {
+    super(message)
+    if (res.error) this.serverError = res.error
+    this.statusCode = res.statusCode
+    this.body = (res.data || {}).message || res.data
+  }
+}
+
+const simpleGet = (url, opts = {}) => new Promise((resolve, reject) => {
+  const { redirect } = opts
+  httpsGet(url, opts, (res) => {
+    const location = res.headers.location
+
+    if (res.statusCode === 302 && redirect !== 'manual' && location) {
+      return simpleGet(location, { redirect }).then(resolve)
+    }
+
+    if (opts.noBody) return resolve(res)
+
+    const isJson = res.headers['content-type'].indexOf('json') !== -1
+    if (isJson) res.setEncoding('utf8')
+
+    let data = ''
+    res.on('data', (d) => { data += d })
+    res.on('end', () => {
+      if (isJson) {
+        try {
+          data = JSON.parse(data)
+        } catch (ex) {
+          res.error = ex
+        }
+      }
+      res.data = data
+      resolve(res)
+    })
+    res.on('error', (error) => resolve({ statusCode: 500, error }))
+  })
+})
+
+const isPrivate = async (repo, token) => {
+  const url = apiBaseUrl(repo)
+
+  const res = await simpleGet(url, { headers: uaJsonHeader })
+  if (res.statusCode === 200) return { private: false }
+
+  if (token) {
+    const headers = getAuthHdrJson(token)
+    const privRes = await simpleGet(url, { headers })
+    if (privRes.statusCode === 200) return { private: true }
+  }
+
+  throw new GithubApiError(`Repo not found on Github: ${repo}`, res)
+}
+
+const initCacheForRepo = (repo) => {
+  repo.cacheByPath = { channel: {}, platform: {}, serverUrl: {} }
+}
+
+const getConfig = async (configIn = {}) => {
+  if (repos[configIn.repos]) return configIn
+
+  const config = Object.assign({}, configIn)
+  config.serverUrl = (config.serverUrl || SERVER_URL || '').trim()
+  config.token = (config.token || GITHUB_OAUTH_TOKEN || '').trim()
+  config.account = (config.account || GITHUB_ACCOUNT || '').trim()
+  config.project = (config.project || GITHUB_PROJECT || '').trim()
+  config.repo = (config.repo || GITHUB_REPO || '').trim()
+  config.platformFilters = config.platformFilters || {}
+
+  if (!config.repo) {
+    if (!(config.account && config.project)) throw new Error(`Repo is required`)
+    config.repo = `${config.account}/${config.project}`
+  }
+
+  if (config.repo.indexOf('github.com/') !== -1) {
+    config.repo = config.repo.replace(/.*github.com\//, '')
+  }
+
+  if (repos[config.repo]) return config
+
+  repos[config.repo] = await isPrivate(config.repo, config.token)
+  initCacheForRepo(repos[config.repo])
+
+  if (repos[config.repo].private && !config.serverUrl) {
+    let msg = `\nFor private repos we recommend setting serverUrl / SERVER_URL\n`
+    msg += ` - If not set we will try to extract that info from each request\n`
+    msg += ` - That isn't guaranteed to produce the correct return URL\n`
+    msg += ` - That also adds overhead to each request\n`
+    console.log(msg)
+  }
+
+  return config
+}
+
+const getReleaseList = async (config) => {
+  try {
+    config = await getConfig(config)
+  } catch (ex) {
+    return Promise.reject(ex)
+  }
+
+  const { repo, token } = config
+  const rlsUrl = `${apiBaseUrl(repo)}/releases`
+  const headers = getAuthHdrJson(token)
+  const { data: releases, error } = await simpleGet(rlsUrl, { headers })
+
+  if (error) return Promise.reject(error)
+
+  repos[repo].releases = releases
+
+  return Promise.resolve(releases)
+}
+
+const latestByChannel = async (config) => {
+  try {
+    config = await getConfig(config)
+  } catch (ex) {
+    return Promise.reject(ex)
+  }
+
+  const { repo } = config
+
+  const releases = repos[repo].releases || await getReleaseList(config)
+  const channels = repos[repo].channels = repos[repo].channels || {}
+
+  const setLatestForChannel = (release, channel) => {
+    release.channel = channel = channel.toLowerCase()
+    const currentTag = (channels[channel] || {}).tag_name
+    if (currentTag && semver.gt(currentTag, release.tag_name)) return
+    channels[channel] = release
+  }
+
+  releases.forEach((r) => {
+    const { tag_name: tag } = r
+    const cleanTag = semver.valid(tag)
+
+    if (!cleanTag) return
+
+    const tagMeta = tag.indexOf('+') >= 0 ? tag.slice(tag.indexOf('+') + 1) : ''
+    const tagPreAry = semver.prerelease(cleanTag) || []
+    const tagPre = tagPreAry.find((p) => Number.isNaN(+p))
+    const rlsPre = r.prerelease ? `prerelease` : null
+
+    const channel = [tagMeta, tagPre, rlsPre].filter((c) => c).join('/')
+    setLatestForChannel(r, channel)
+  })
+
+  return channels
+}
+
+const guessPlatform = (ua = '') => {
+  const uaPlatform = ((ua.match(/\((.+?)\)/) || [])[1] || '').toLowerCase()
+
+  if (uaPlatform && uaPlatform.indexOf('windows') !== -1) return 'win32'
+  if (uaPlatform && uaPlatform.indexOf('mac') !== -1) return 'darwin'
+
+  return null
+}
+
+const getPlatformUrl = () => {}
+
+const getUpdateUrl = () => {}
+
+const getReleasesUrl = () => {}
+
+const getDownloadUrl = () => {}
+
+const redirectToUrl = () => {}
+
+const getChannel = (repo, channels, pathLower) => {
+  const cached = repo.cacheByPath.channel[pathLower]
+  if (cached) return cached
+
+  let channel
+  Object.entries(channels).forEach(([channelName, release]) => {
+    if (!channelName || pathLower.indexOf(channelName) === -1) return
+    if (!channel) return (channel = release)
+
+    const slashLen = (name) => name.split('/').length
+    const useCurrent = slashLen(channel.channel) > slashLen(channelName)
+    channel = useCurrent ? channel : release
+  })
+
+  channel = channel || channels['']
+  repo.cacheByPath.channel[pathLower] = channel
+
+  return channel
+}
+
+const getPlatform = (repo, pathLower, channel, headers) => {
+  const cached = repo.cacheByPath.platform[pathLower]
+  if (cached) return cached
+
+  let tmpPath = pathLower.replace(/^\/download|update/, '')
+  if (channel.channel) tmpPath = tmpPath.replace(`/${channel.channel}`, '')
+
+  const platform = tmpPath.split('/')[1] || guessPlatform(headers['user-agent'])
+  repo.cacheByPath.platform[pathLower] = platform
+
+  return platform
+}
+
+const getServerUrl = (repo, pathLower, req) => {
+  const cached = repo.cacheByPath.serverUrl[pathLower]
+  if (cached) return cached
+
+  const { socket } = req
+  const unsecure = socket.localPort === 80 || socket.remotePort === 80
+
+  if (!req.headers.host) return
+
+  const serverUrl = `${unsecure ? 'http' : 'https'}://${req.headers.host}`
+  repo.cacheByPath.serverUrl[pathLower] = serverUrl
+
+  return serverUrl
+}
+
+const filterByExt = (assets, ext) => {
+  ext = ext.charAt(0) === '.' ? ext : `.${ext}`
+  const extLen = ext.length
+  return assets.filter((a) => a.name.indexOf(ext) === a.name.length - extLen)
+}
+
+const firstForArch = (assets, arch) => {
+  assets.sort((a, b) => {
+    a = a.name.indexOf('64') === -1 ? -1 : 1
+    b = b.name.indexOf('64') === -1 ? -1 : 1
+    return a - b
+  })
+
+  return arch === 'x64' ? assets[assets.length - 1] : assets[0]
+}
+
+const platformFilters = {
+  win32: (assets, action = 'download', arch, ext) => {
+    const download = () => {
+      ext = ext || 'exe'
+      if ((assets = filterByExt(assets, ext)).length < 2) return assets[0]
+      return firstForArch(assets, arch)
+    }
+
+    const update = () => {
+      ext = ext || 'zip'
+      if ((assets = filterByExt(assets, ext)).length < 1) return download()
+
+      assets = assets.filter((a) => a.name.match(/windows|win32|win64/i))
+      return firstForArch(assets, arch)
+    }
+
+    const release = () => {
+      assets = assets.filter((a) => !a.name.indexOf('RELEASES'))
+      if ((assets = filterByExt(assets, ext)).length < 2) return assets[0]
+      return firstForArch(assets, arch)
+    }
+
+    const actions = { download, update, release }
+
+    return actions[action] ? actions[action]() : null
+  }
+}
+
+const getPlatformAsset = (config, repo, channel, platform, action, arch) => {
+  const assets = channel.assets.slice(0)
+  let asset
+
+  if (!config.platformFilters[platform] && !platformFilters[platform]) return
+
+  if (config.platformFilters && config.platformFilters[platform]) {
+    asset = config.platformFilters[platform](assets, action, arch)
+  }
+
+  asset = asset || platformFilters[platform](assets, action, arch)
+
+  return asset
+}
 
 const requestHandler = async (config) => {
   try {
@@ -72,4 +366,14 @@ const requestHandler = async (config) => {
   }
 }
 
-module.exports = requestHandler
+module.exports = {
+  getReleaseList,
+  latestByChannel,
+  requestHandler,
+  guessPlatform,
+  getPlatformUrl,
+  getUpdateUrl,
+  getReleasesUrl,
+  getDownloadUrl,
+  redirectToUrl
+}
